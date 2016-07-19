f17afc8da18c36a1dd199a0f08699e028db44c29
Emil Hemdal <emil.hemdal.em@gmail.com>
2016-07-19 17:11:27 +0200

    Initial login functionality and pug rendering
    Two login alternatives added (email + password or organization + username + password).
    Initial files for pug rendering.
    Initial MySQL fetching of data.

--- server.js
+++ server.js
@@ -1,45 +1,127 @@
 
+/**
+ *
+ * Proxive
+ *
+ * Copyright 2016 Landstinget Dalarna Bibliotek och Informationscentral
+ * Copyright 2016 Emil Hemdal <https://github.com/emilhem>
+ *
+ * Proxive is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proxive is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Proxive.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 'use strict';
 
-var http        = require('http');
-var Unblocker   = require('unblocker');
-var fs          = require('fs');
-var path        = require('path');
-var pugGen      = require('./lib/pugGenerator.js');
+var http          = require('http');
+var Cookies       = require('cookies');
+var Unblocker     = require('unblocker');
+var mysql         = require('mysql');
+var fs            = require('fs');
+var qs            = require('querystring');
+var url           = require('url');
+var path          = require('path');
 
-var specialRedirects = require('./lib/special-redirects.js');
 
 function setupServer(config) {
+
+  var pool = mysql.createPool({
+    connectionLimit:  10,
+    host:             config.mysql.host,
+    port:             (config.mysql.port | 3306),
+    user:             config.mysql.user,
+    password:         config.mysql.pass,
+    database:         config.mysql.database,
+    charset:          'utf8mb4'
+  });
+
+  pool.getConnection(function(err, connection) {
+    if(err) {
+      console.error(err);
+      throw 'MySQL not set up properly!';
+    }
+    connection.query('SHOW TABLES;', function(err) {
+      if(err) {
+        console.error(err);
+        throw 'MySQL not set up properly!';
+      }
+      console.log('MySQL activated!');
+      connection.release();
+    });
+  });
+
+  config.mysql = pool;
+
+  config.organizations = require('./lib/organizations.js')(config);
+  config.users = require('./lib/users.js')(config);
+
+  config.pugGen = require('./lib/pugGenerator.js')(config);
+
+  var get  = require('./lib/get.js')(config);
+  var post = require('./lib/post.js')(config);
+
   var unblockerConfig = {
     prefix: false,
     domainPrefixing: true,
     domain: config.proxyServer.domain
   };
 
-  unblockerConfig.requestMiddleware = [specialRedirects(unblockerConfig)];
+  unblockerConfig.requestMiddleware = [require('./lib/special-redirects.js')(unblockerConfig)];
 
   var unblocker = new Unblocker(unblockerConfig);
 
   // Proxy server
   http.createServer(function(req, res) {
-    // TODO: check logincookies!
-    unblocker(req, res, function(err) {
-      // this callback will be fired for any request that unblocker does not serve
-      var headers = {'content-type': 'text/html'};
-      if (err) {
-        console.error(err.stack || err.message);
-        console.error(err);
-        res.writeHead(500, headers);
-        return res.end(pugGen.run['500.pug']({title: 'Ett fel hände vid hämtandet av sidan!', message: 'Ett fel inträffade! Är du säker på att adressen ska fungera? Kontakta i så fall emil.hemdal@ltdalarna.se'}));
-      }
-      res.writeHead(404, headers);
-      return res.end(pugGen.run['404.pug']({title: 'Ett fel hände vid hämtandet av sidan!', message: 'Ett fel inträffade! Är du säker på att adressen ska fungera? Kontakta i så fall emil.hemdal@ltdalarna.se'}));
-    });
+
+    var clientCookies = new Cookies( req, res );
+
+    if(organizations.has(clientCookies.get('org')) && clientCookies.get('pw', {signed: true}) === '1337') {
+
+      req.ipToConnectWith = organizations.get(clientCookies.get('org'));
+
+      // TODO: check logincookies!
+      unblocker(req, res, function(err) {
+        // this callback will be fired for any request that unblocker does not serve
+        var headers = {'content-type': 'text/html'};
+        if (err) {
+          console.error(err.stack || err.message);
+          console.error(err);
+          res.writeHead(500, headers);
+          return res.end(config.pugGen.run['500.pug']({title: 'Ett fel hände vid hämtandet av sidan!',
+            message: 'Ett fel inträffade! Är du säker på att adressen ska fungera? Kontakta i så fall emil.hemdal@ltdalarna.se'}));
+        }
+        res.writeHead(404, headers);
+        return res.end(config.pugGen.run['404.pug']({title: 'Ett fel hände vid hämtandet av sidan!',
+          message: 'Ett fel inträffade! Är du säker på att adressen ska fungera? Kontakta i så fall emil.hemdal@ltdalarna.se'}));
+      });
+    } else {
+      res.writeHead(302, {
+        location: 'http://u.fabicutv.com/'
+      });
+      res.end();
+    }
   }).listen(config.proxyServer.port, '127.0.0.1');
 
   // Loginserver
   http.createServer(function(req, res) {
-    res.end('TODO');
+    req.cookies = new Cookies( req, res );
+    if(req.method === 'POST') {
+      post(req, res);
+    } else if(req.method === 'GET') {
+      get(req, res);
+    } else {
+      req.end('Method not supported!');
+    }
   }).listen(config.loginServer.port, '127.0.0.1');
 }
 

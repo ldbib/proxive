3cec149d19d586679d0980bf1e751f90676ae9e3
Emil Hemdal <emil.hemdal.em@gmail.com>
2016-08-10 10:31:10 +0200

    Improved loginpage
    Changed the layout and added labels to the input fields.
    Now the user can select what way they want to sign in. Email or username.

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+/* jshint browser: true, node: false */
+
+(function() {
+  'use strict';
+
+  document.getElementById('loginUsernameButton').onclick = function() {
+    document.getElementById('loginSelector').style.display = 'none';
+    document.getElementById('loginForms').style.display = 'block';
+    document.getElementById('loginUsername').style.display = 'block';
+    document.getElementById('organization').focus();
+  };
+  document.getElementById('loginEmailButton').onclick = function() {
+    document.getElementById('loginSelector').style.display = 'none';
+    document.getElementById('loginForms').style.display = 'block';
+    document.getElementById('loginEmail').style.display = 'block';
+    document.getElementById('emailE').focus();
+  };
+  document.getElementById('loginBack').onclick = function() {
+    document.getElementById('loginSelector').style.display = 'block';
+    document.getElementById('loginForms').style.display = 'none';
+    document.getElementById('loginUsername').style.display = 'none';
+    document.getElementById('loginEmail').style.display = 'none';
+    document.getElementById('loginUsernameButton').focus();
+  };
+  document.getElementById('createNewAccount').onclick = function() {
+    window.location = '/signup';
+  };
+})();

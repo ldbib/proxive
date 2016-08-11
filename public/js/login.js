5cef3130e10d5b8276375af10c605406237e248b
Emil Hemdal <emil.hemdal.em@gmail.com>
2016-08-11 17:16:09 +0200

    Add more buttons on login page
    For password recovery and Walk In Use.
    Also added a signin form for Walk In Use.

--- public/js/login.js
+++ public/js/login.js
@@ -15,14 +15,24 @@
     document.getElementById('loginEmail').style.display = 'block';
     document.getElementById('emailE').focus();
   };
+  document.getElementById('wiuButton').onclick = function() {
+    document.getElementById('loginSelector').style.display = 'none';
+    document.getElementById('loginForms').style.display = 'block';
+    document.getElementById('wiu').style.display = 'block';
+    document.getElementById('emailE').focus();
+  };
   document.getElementById('loginBack').onclick = function() {
     document.getElementById('loginSelector').style.display = 'block';
     document.getElementById('loginForms').style.display = 'none';
     document.getElementById('loginUsername').style.display = 'none';
     document.getElementById('loginEmail').style.display = 'none';
+    document.getElementById('wiu').style.display = 'none';
     document.getElementById('loginUsernameButton').focus();
   };
   document.getElementById('createNewAccount').onclick = function() {
     window.location = '/signup';
+  };
+  document.getElementById('forgotButton').onclick = function() {
+    window.location = '/remindMe';
   };
 })();
